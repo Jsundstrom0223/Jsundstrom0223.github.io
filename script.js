c5f00d6aebbceb2473d3394707cf248cf7ebfd60
Jolene Sundstrom <jolene.sundstrom@gmail.com>
2025-04-21 19:54:39 -0400

    Added startQuestions()

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ function hiThere() {
     let name = prompt("What's your name?", "Gill");
     alert(`Hi ${name}`);
     let answer = confirm("Would you like to be on time?");
+    if (answer) {startQuestions()}
+}
+
+function startQuestions() {
+    let timeclaim = +prompt("On average, how long does it take you to get ready (from '0% ready' to 'ready to walk out the door'?", 20) + 10;
+
+    alert(`A likely story. Let's call it ${timeclaim}`);
+
 }
