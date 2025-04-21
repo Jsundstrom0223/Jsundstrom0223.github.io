3cb68ca536fad4019f92321495f29fcbfb55e762
Jolene Sundstrom <jolene.sundstrom@gmail.com>
2025-04-20 21:04:31 -0400

    hiThere change

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function hiThere() {
-    alert("Function declared in external JS");
+ 
     let name = prompt("What's your name?", "Gill");
     alert(`Hi ${name}`);
     let answer = confirm("Would you like to be on time?");
